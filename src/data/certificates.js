536d74c9e20e7c87a1d00b5bc8a34b7836641337
Arseny Prosvetov <arsprosvet@gmail.com>
2025-06-27 04:18:57 +0300

    Add API, JavaScript, CSS and HTML certificates (27.06.2025) from Gosuslugi

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -1,10 +1,44 @@
 import IconLink from '@/assets/icons/IconLink.svg'
+import IconHtml from '@/assets/icons/IconHtml.svg'
 import IconCss from '@/assets/icons/IconCss.svg'
 import IconGitHub from '@/assets/icons/IconGitHub.svg'
 import IconJs from '@/assets/icons/IconJs.svg'
+import IconApi from '@/assets/icons/IconApi.svg'
 import IconReact from '@/assets/icons/IconReact.svg'
 
 const certificates = [
+  {
+    label: 'Госуслуги',
+    value: 'API - Продвинутый уровень',
+    date: 'Июнь 2025 – Июнь 2026',
+    icon: IconApi,
+    iconLink: IconLink,
+    link: 'https://drive.google.com/file/d/12qOAj0fybc8j629doF-IhNq5iIuAhNcS/view?usp=drive_link'
+  },
+  {
+    label: 'Госуслуги',
+    value: 'JavaScript - Продвинутый уровень',
+    date: 'Июнь 2025 – Июнь 2026',
+    icon: IconJs,
+    iconLink: IconLink,
+    link: 'https://drive.google.com/file/d/1jP2VfXxlkePyUm2dPsM7xS2x5-LvZNJT/view?usp=drive_link'
+  },
+  {
+    label: 'Госуслуги',
+    value: 'CSS - Продвинутый уровень',
+    date: 'Июнь 2025 – Июнь 2026',
+    icon: IconCss,
+    iconLink: IconLink,
+    link: 'https://drive.google.com/file/d/1xDkxQ3gdVlYyJHaMnJ1_BVRN9BMBTr32/view?usp=drive_link'
+  },
+  {
+    label: 'Госуслуги',
+    value: 'HTML - Продвинутый уровень',
+    date: 'Июнь 2025 – Июнь 2026',
+    icon: IconHtml,
+    iconLink: IconLink,
+    link: 'https://drive.google.com/file/d/1qCaHaXP6qmqw87OX5LDWcw5eKnod_7Pn/view?usp=drive_link'
+  },
   {
     label: 'Нетология',
     value: 'Адаптивная и мобильная верстка',
